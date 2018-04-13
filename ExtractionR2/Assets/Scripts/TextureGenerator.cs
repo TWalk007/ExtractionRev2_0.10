8913deade28be958845958e3f47cbd1a4c41e924
TWalk007 <walkertb03@gmail.com>
2018-04-13 16:08:58 -0400

    Small Update to texture Size

--- ExtractionR2/Assets/Scripts/TextureGenerator.cs
+++ ExtractionR2/Assets/Scripts/TextureGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class TextureGenerator {
 
     public static Texture2D TextureFromColorMap(Color[] colorMap, int width, int height) {
-        Texture2D texture = new Texture2D(width, height);
+        Texture2D texture = new Texture2D(width , height);
         texture.filterMode = FilterMode.Point;
         texture.wrapMode = TextureWrapMode.Clamp;
         texture.SetPixels(colorMap);
