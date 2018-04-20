c3b3685dc2063201982e32b6f56662c39bf9ad67
TWalk007 <walkertb03@gmail.com>
2018-04-20 09:00:19 -0400

    Working to populate regions tex data

--- ExtractionR2/Assets/Scripts/MapGeneratorLague.cs
+++ ExtractionR2/Assets/Scripts/MapGeneratorLague.cs
@@ -59,20 +59,35 @@ public class MapGeneratorLague : MonoBehaviour {
             // this is where we'll place the Texture2D that is in the "Terrain Tex" slot of the inspector for each region.
             // First, we need to store each of the Texture2D pixel info into an array.
 
-            //TESTING ONLY///////////////////////////////////////////
-            TileToPixels(regions[1].terrainTex);
+            Color[] texMap = new Color[textureSizeDPI ^ 2];
+            texMap = regions[1].terrainTex.GetPixels();
+            Debug.Log(texMap[5]);
 
+            //  The values have correctly been store in the color array!!!  Woot!!
+            //  Now I just need apply this color[] data (texMap) to the correct spot in the colorMap above.
+            //  Or more precisely create my own Color Map so I don't overwrite the noise color map as it's still useful.
 
-            for (int y = 0; y < mapHeight; y++) {
+            for (int y = 0; y < mapHeight; y++) { // we don't " * texureSizeDPI " here because we are indexing based upon the noise map.  We add that further down to accommodate the larger size.
                 for (int x = 0; x < mapWidth; x++) {
-                    float currentHeight = noiseMap[x, y];
-
                     for (int i = 0; i < regions.Length; i++) {
-                        if (currentHeight <= regions[i].height) {
-                            colorMap[y * mapWidth + x] = regions[i].color;
+                        // Below means "current pixel within the noise map.  This is relevant because we are using this pixel as our whole tile).  one tile per pixel.
+                        float currentHeight = noiseMap[x, y];
+
+                        // Then for each tile, we will add the terrain's texture data to the new terrainTextures array (our Texture2D).
+                        if (regions[i].terrainTex == null) {
+                            Debug.Log(regions[i].name + " is missing a terrain texture!");
+                        } else if (regions[i].terrainTex != null) {
+
+                        /* This is where I need to place the texMap array into the correct index of the terrainTextures array.
+                        I need to make sure if it's the first slot of the terrainTextures array it goes to terrainTextures[0], otherwise
+                        it should just be an "array.add".  The problem with this is that it will add arrays into the slots of where
+                        any missing arrays should go.So...it would be better to insert the texMap array into the correct index instead
+                        of just doing an array.add.Not sure yet how to do this.
+                        */
+                            //terrainTextures[y * textureSizeDPI + x] = texMap; ;
                             break;
-                        }
-                    }
+                        }                           
+                    }                    
                 }
             }
         }
@@ -89,25 +104,6 @@ public class MapGeneratorLague : MonoBehaviour {
 
     // TileToPixels () is a method that will use a specified Texture2D from the regions [] and use it to populate a color array.
     // We will use this data later to populate a single tile on the larger texture map (our tile map).
-    private Color[] TileToPixels (Texture2D tileTex) {
-        Color[] singleTilePixels = new Color[textureSizeDPI^2];
-
-        for (int y = 0; y < textureSizeDPI; y++) {
-            for (int x = 0; x < textureSizeDPI; x++) {
-
-                float currentHeight = tileTex.GetPixel(x,y).grayscale;
-
-                singleTilePixels[y * textureSizeDPI + x] = tileTex.GetPixel(x,y);
-
-                Debug.Log(singleTilePixels);
-                break;
-                
-            }
-        }
-
-
-        return singleTilePixels;
-    }
 
 
 
